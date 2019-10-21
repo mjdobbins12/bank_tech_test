d254c1c605c34876c8cca520386810a44e140bdb
Mark Dobbins <mjdobbins12@gmail.com>
2019-10-21 17:56:29 +0100

    currency values stored as number of cents

--- spec/bankSpec.js
+++ spec/bankSpec.js
@@ -9,12 +9,20 @@ describe('Bank', function() {
 
   it('allows deposits to be made', function() {
     bank.deposit(100);
-    expect(bank.balance).toEqual(100);
+    expect(bank.balance).toEqual(10000);
   })
 
   it('allows money to be withdrawn', function() {
     bank.deposit(100);
     bank.withdraw(50);
-    expect(bank.balance).toEqual(50);
+    expect(bank.balance).toEqual(5000);
+  })
+
+  it('displays a record of transactions', function() {
+    let exampleStatement = "date \|\| credit \|\| debit \|\| balance\n21\/10\/2019 \|\| \|\| 75.00 \|\| 25.00\n20\/10\/2019 \|\| 100.00 \|\|  \|\| 100.00"
+
+    bank.deposit(100, '20/10/2019');
+    bank.withdraw(75, '21/10/2019');
+    expect(bank.statement).toEqual(exampleStatement)
   })
 })
--- src/bank.js
+++ src/bank.js
@@ -1,13 +1,16 @@
 class Bank {
   constructor() {
     this.balance = 0;
+    this.transactions = []
   }
 
-  deposit(amount) {
-    this.balance += amount;
+  deposit(amount, date = moment().format('L')) {
+    this.balance += (amount * 100);
+    this.transactions.unshift([date, amount]);
   }
 
-  withdraw(amount) {
-    this.balance -= amount;
+  withdraw(amount, date = moment().format('L')) {
+    this.balance -= (amount * 100);
+    this.transactions.unshift([date, amount]);
   }
 }
